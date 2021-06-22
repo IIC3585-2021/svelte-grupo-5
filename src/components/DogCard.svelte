46ee08420cc26b1a42902a1d7a6b90f92ea1aa21
sofiadelano <smdelano@uc.cl>
2021-06-22 14:55:25 -0400

    add basic structure to  dogcard

--- src/components/DogCard.svelte
+++ src/components/DogCard.svelte
@@ -1,9 +1,51 @@
 <script>
-  //   export let name;
+  export let dog;
+  export let favorite;
+  export let filtered;
+
+  const moreInfo = () => {
+    console.log("see more button clicked");
+  };
+  const removeFromFavorites = (dog) => {
+    console.log("remove from favorites button clicked");
+  };
+  const addToFavorites = (dog) => {
+    console.log("add to favorites button clicked");
+  };
 </script>
 
 <main>
   <img id="image" alt="dog" />
+  <!-- info -->
+  {#if favorite}
+    <div id="info-favorite">
+      <h4>Breed: {dog.bred_for || "None"}</h4>
+      <h4>Group: {dog.breed_group || "None"}</h4>
+      <p>-</p>
+    </div>
+  {:else}
+    <div id="info-not-favorite">
+      <h4>Breed: {dog.bred_for || "None"}</h4>
+      <h4>Group: {dog.breed_group || "None"}</h4>
+    </div>
+  {/if}
+  <!-- buttons -->
+  {#if favorite}
+    <button on:click={moreInfo}>See More</button>
+    <button on:click={removeFromFavorites(dog)}>Delete from Favorites</button>
+  {/if}
+  {#if filtered}
+    <button on:click={addToFavorites(dog)} onclick="this.disabled=true"
+      >Add to Favorites</button
+    >
+  {/if}
+  {#if favorite == false}
+    {#if filtered == false}
+      <button on:click={addToFavorites(dog)} onclick="this.disabled=true"
+        >Add to Favorites</button
+      >
+    {/if}
+  {/if}
 </main>
 
 <style>
@@ -20,6 +62,20 @@
   img {
     display: "flex";
     height: "200px";
+    width: "200px";
+    margin: "20px";
+  }
+  #info-favorite {
+    display: "flex";
+    flex-direction: "column";
+    height: "auto";
+    width: "200px";
+    margin: "20px";
+  }
+  #info-not-favorite {
+    display: "flex";
+    flex-direction: "column";
+    height: "150px";
     width: "200px";
     margin: "20px";
   }
--- src/components/Dogs.svelte
+++ src/components/Dogs.svelte
@@ -1,10 +1,19 @@
 <script>
+  import { each } from "svelte/internal";
   import DogCard from "./DogCard.svelte";
+  let dogs = [
+    { bred_for: "hola", breed_group: "hola" },
+    { bred_for: "chao", breed_group: "chao" },
+    { bred_for: "wena", breed_group: "wena" },
+    { bred_for: "jijij", breed_group: "jijiji" },
+  ];
 </script>
 
 <main>
   <h1>Dogs List</h1>
-  <DogCard />
+  {#each dogs as dog}
+    <DogCard favorite={false} filtered={false} {dog} />
+  {/each}
 </main>
 
 <style>
